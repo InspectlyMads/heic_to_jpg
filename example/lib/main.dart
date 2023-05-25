fd191e8ae002980abbf7c16fa30dcea1eeffa8a7
Mads Stoltenborg <mfs@ikontrol.dk>
2023-05-25 11:00:27 +0200

    Upgrade versions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:heic_to_jpg/heic_to_jpg.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,8 +8,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _path;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,39 +18,10 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                child: const Text('Pick a file...'),
-                onPressed: _pickFile,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: _path != null
-                    ? Image.file(
-                        File(_path),
-                        width: 300.0,
-                        height: 300.0,
-                        fit: BoxFit.cover,
-                      )
-                    : Container(height: 300.0),
-              ),
-            ],
+            children: <Widget>[],
           ),
         ),
       ),
     );
-  }
-
-  void _pickFile() async {
-    final path = await FilePicker.getFilePath(
-        type: FileType.CUSTOM, fileExtension: "HEIC");
-    if (path != null) {
-      final jpgPath = await HeicToJpg.convert(path);
-      if (jpgPath != null) {
-        setState(() {
-          _path = jpgPath;
-        });
-      }
-    }
   }
 }
